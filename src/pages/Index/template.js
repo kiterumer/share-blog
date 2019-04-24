b412e2179c770cbe4c421f11b54308eb572943ca
kiterumer <1404646092@qq.com>
2019-04-24 20:08:50 +0800

    完成静态页面

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,8 +1,31 @@
+import request from '@/helpers/request.js'
+import auth from '@/api/auth.js'
+import blog from '@/api/blog.js'
+
 export default {
     // name: 'HelloWorld',
     data () {
       return {
         msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    methods:{
+      success(){
+        this.$message({
+          message:'这是一条成功消息',
+          type:'success'
+        })
+      },
+      click1(){
+        this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${ action }`
+            })
+          }
+        })
       }
     }
   }
